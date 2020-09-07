c9e48921100d35346ec2a5e15aec715c34e7adbe
HannahSav <70842483+HannahSav@users.noreply.github.com>
2020-09-07 10:35:48 +0300

    Add files via upload

---
+++ card.php
@@ -0,0 +1,13 @@
+
+<?php
+
+var_dump($_POST);
+/*if (session_id() === "") {
+   session_start();
+}
+$start = microtime(true);
+
+date_default_timezone_set("UTC");
+$time = time() + 3 * 3600; 
+echo "<p id='time'>Текущее время: ".date("H:i:s", $time)."</p>";
+$y=(float)$_POST['y'];*/
